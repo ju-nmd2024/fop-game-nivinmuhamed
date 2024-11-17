a919e7a6d2aa8a2f9cc7a20834fcc467e51aea2f
nivinmuhamed <muni24by@student.ju.se>
2024-11-17 22:11:00 +0100

    Update game.js
    i forgot to commit one to many times... i have created different screens and put in my character

--- game.js
+++ game.js
@@ -1,8 +1,113 @@
+//christmas tree game
+// Nivin Muhamed, muni24by
+
 function setup() {
   createCanvas(800, 600);
 }
 
-function draw() {
-  background(255, 140, 0);
+let treeX = 400;
+let treeY = 200;
+let state = "start";
+
+//game logic
+let velocityY = 1;
+let acceleration = 1;
+
+//start screen game
+function startScreen() {
+  background(95, 15, 22);
+  noStroke();
+  textSize(50);
+  text("Start", 210, 210);
 }
-//vad som helst 
+
+function christmasOrnaments() {
+  stroke(0);
+  strokeWeight(2);
+  line(100, 0, 100, 115);
+  line(180, 0, 180, 70);
+  line();
+}
+
+//game screen
+function gameScreen() {
+  background(95, 15, 22);
+  noStroke();
+  text("Game", 210, 210);
+  tree(treeX,treeY);
+}
+
+function lostScreen() {
+  background(100, 200, 200);
+  noStroke();
+  text("You lost", 210, 210);
+}
+
+function wonScreen() {
+  background(100, 100, 100);
+  noStroke();
+  text("You won!", 210, 210);
+}
+
+function tree(x, y) {
+  //tree
+  push();
+  scale(0.56);
+  fill(53, 33, 0);
+  rect(x + 70, y + 200, 30, 50);
+  fill(0, 100, 0);
+  noStroke();
+  triangle(x + 90, y - 70, x + 40, y - 15, x + 140, y - 15);
+  triangle(x + 90, y - 50, x + 15, y + 45, x + 170, y + 45);
+  triangle(x + 90, y - 20, x - 20, y + 120, x + 200, y + 120);
+  triangle(x + 90, y, x - 55, y + 200, x + 233, y + 200);
+  //decorations
+  fill(255, 205, 60);
+  triangle(x + 90, y - 89, x + 72, y - 60, x + 106, y - 60);
+  triangle(x + 75, y - 80, x + 105, y - 80, x + 90, y - 50);
+  fill(195, 15, 22);
+  ellipse(x + 150, y + 155, 15);
+  ellipse(x + 25, y + 105, 15);
+  ellipse(x + 65, y + 20, 15);
+  fill(192, 192, 192);
+  ellipse(x + 85, y + 155, 15);
+  ellipse(x + 120, y + 50, 15);
+  fill(206, 172, 92);
+  ellipse(x + 80, y + 90, 15);
+  ellipse(x + 30, y + 180, 15);
+  pop();
+}
+
+function draw() {
+  //starting screen
+  if (state === "start") {
+    startScreen();
+  } else if (state === "game") {
+    gameScreen();
+
+    //christmas tree falling
+    if (tree >= 1210) {
+      if (velocity <= 5) {
+        state = "won";
+      } else if (velocity > 5) {
+        state = "lost";
+      }
+    }
+  }
+}
+
+// gravity logic
+treeY = treeY + velocityY;
+velocityY = velocityY + acceleration;
+
+//space key, it will control the acceleration
+if (keyIsDown(32) === true) {
+  acceleration = -1;
+} else {
+  acceleration = 0.5;
+}
+
+function draw() {
+  gameScreen();
+  christmasOrnaments();
+}
