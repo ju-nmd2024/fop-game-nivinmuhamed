e007204bb4f81243260a46fd35110e8f68807231
nivinmuhamed <muni24by@student.ju.se>
2024-11-18 23:09:39 +0100

    Update game.js
    I have now put in mouseClicked and put in velocity and acceleration. will now countinue with making it pretty lol

--- game.js
+++ game.js
@@ -11,77 +11,102 @@ let state = "start";
 
 //game logic
 let velocityY = 1;
-let acceleration = 1;
+let acceleration = 0.5;
 
 //start screen game
 function startScreen() {
   background(95, 15, 22);
-  christmasOrnaments ();
+  christmasOrnaments();
   noStroke();
   textSize(50);
   push();
   fill(30, 121, 44);
-  rect(135, 155, 315, 130, 20);
+  rect(240, 155, 315, 130, 20);
   pop();
-  text("Click to start", 150, 235);
+  text("Click to start", 260, 235);
 }
 
-// colors, red - 195, 15, 22, gold - 255, 205, 60, grey - 192, 192, 192, 
+// colors, red - 195, 15, 22, gold - 255, 205, 60, grey - 192, 192, 192,
 function christmasOrnaments() {
   stroke(0);
   strokeWeight(2);
+  //gold
   fill(255, 205, 60);
   line(30, 0, 30, 75);
   ellipse(30, 75, 20);
+  //red
   fill(195, 15, 22);
   line(100, 0, 100, 115);
   ellipse(100, 115, 30);
   fill(192, 192, 192);
   line(175, 0, 175, 70);
   ellipse(175, 70, 20);
+  //gold
   fill(255, 205, 60);
   line(235, 0, 235, 90);
   ellipse(235, 90, 30);
   fill(195, 15, 22);
   line(305, 0, 305, 120);
   ellipse(305, 120, 25);
+  //grey
   fill(192, 192, 192);
+  line(610, 0, 610, 90);
+  ellipse(610, 90, 25);
   line(370, 0, 370, 80);
   ellipse(370, 80, 32);
+  //gold
   fill(255, 205, 60);
   line(450, 0, 450, 115);
   ellipse(450, 115, 27);
+  line(690, 0, 690, 120);
+  ellipse(690, 120, 30);
+  //red
   fill(195, 15, 22);
   line(535, 0, 535, 60);
   ellipse(535, 60, 20);
+  line(770, 0, 770, 110);
+  ellipse(770, 110, 35);
+}
+
+//christmastree pot and fireplace
+function potAndFireplace() {
+  fill(84, 54, 32);
+  rect(0, 600, 800, 600);
 }
 
 //game screen
 function gameScreen() {
   background(95, 15, 22);
-  noStroke();
-  textSize(35);
-  push();
-  fill(30, 121, 44);
-  rect(210, 155, 130, 70, 20);
-  pop();
-  push();
-  fill(95, 15, 22);
-  text("Game", 227, 200);
-  pop();
-  tree(treeX,treeY);
+  tree(treeX, treeY);
+  christmasOrnaments();
+  treeY = treeY + velocityY;
+  velocityY = velocityY + acceleration;
+  if (treeY >= 600) {
+    if (velocityY <= 5) {
+      state = "won";
+    } else if (velocityY > 5) {
+      state = "lost";
+    }
+  }
 }
 
 function lostScreen() {
-  background(100, 200, 200);
+  background(95, 15, 22);
   noStroke();
-  text("You lost", 210, 210);
+  rect();
+  text("You lost", 300, 235);
+  christmasOrnaments();
+  treeY = -200;
+  velocityY = 0;
 }
 
 function wonScreen() {
-  background(100, 100, 100);
+  background(95, 15, 22);
   noStroke();
   text("You won!", 210, 210);
+  christmasOrnaments();
+  treeY = -200;
+  velocityY = 0;
 }
 
 function tree(x, y) {
@@ -117,28 +142,40 @@ function tree(x, y) {
 treeY = treeY + velocityY;
 velocityY = velocityY + acceleration;
 
-//space key, it will control the acceleration
-if (keyIsDown(32) === true) {
-  acceleration = -1;
-} else {
-  acceleration = 0.5;
-}
-
 function draw() {
   //starting screen
   if (state === "start") {
     startScreen();
   } else if (state === "game") {
     gameScreen();
+  } else if (state === "lost") {
+    lostScreen();
+  } else if (state === "won") {
+    wonScreen();
+  }
 
-    //christmas tree falling
-    if (tree >= 1210) {
-      if (velocity <= 5) {
-        state = "won";
-      } else if (velocity > 5) {
-        state = "lost";
-      }
-    }
+  if (treeY >= 600 && velocityY <= 5) {
+    state = "won";
+  }
+  if (treeY >= 600 && velocityY >= 5) {
+    state = "lost";
+  }
+  //space key, it will control the acceleration
+  if (keyIsDown(UP_ARROW) === true) {
+    velocityY = velocityY - acceleration * 2;
   }
 }
 
+function mouseClicked() {
+  if (state === "start") {
+    state = "game";
+  } else if (state === "game") {
+    state = "lost";
+  } else if (state === "lost") {
+    state = "start";
+  } else if (state === "won") {
+    state = "start";
+  }
+  console.log(state);
+  console.log(velocityY);
+}
